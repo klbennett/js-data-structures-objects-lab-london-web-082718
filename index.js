ab3cc400c900aa2d7242f2987737d02f40ec712a
Kay B <kaylouisebennett@gmail.com>
2018-10-09 22:11:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 
-const drivers = 'isji'
+const driver = { key: value}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
 const newDriver = Object.assign({}, driver[key] = value);
